be9924ffdf21efbc6b003de12a35306088e9db31
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-29 22:13:58 +0200

    Turn on prr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    // ppr: true,
+    ppr: true,
     reactCompiler: true,
     // staleTimes: {
     //   dynamic: 30,
