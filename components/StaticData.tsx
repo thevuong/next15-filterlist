7c85b8fe7041af2a52ca9f69ec2e289820820e11
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-30 23:10:19 +0200

    Update static data

--- components/StaticData.tsx
+++ components/StaticData.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { getStaticData } from '@/data/getData';
 
 export default async function StaticData() {
-  console.log('StaticData');
-  // const data = await getStaticData();
+  const data = await getStaticData();
 
-  return <div className="bg-blue-500 p-4 text-white">Statisk data</div>;
+  return (
+    <div className="bg-blue-500 p-4 text-white">
+      Statisk data
+      {data[0].id}
+    </div>
+  );
 }
