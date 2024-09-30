e4282d2c68b64cb651e24f013052ed5842b520ef
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-30 23:28:56 +0200

    Update

--- components/StaticData.tsx
+++ components/StaticData.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
+import { slow } from '@/utils/slow';
 
 export default async function StaticData() {
-  await new Promise(() => {
-    setTimeout(() => {}, 1000);
-  });
-  console.log('StaticData');
+  await slow();
 
   return (
     <div className="bg-blue-500 p-4 text-white">
