80b68f31193c819a579c1c40be616cad6a5ef049
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-30 22:56:49 +0200

    try fix ppr

--- components/StaticData.tsx
+++ components/StaticData.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { getStaticData } from '@/data/getData';
 
 export default async function StaticData() {
-  const data = await getStaticData();
+  console.log('StaticData');
+  // const data = await getStaticData();
 
-  return (
-    <div className="bg-blue-500 p-4 text-white">
-      Statisk data
-      {data[0].id}
-    </div>
-  );
+  return <div className="bg-blue-500 p-4 text-white">Statisk data</div>;
 }
