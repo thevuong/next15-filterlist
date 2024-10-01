04fd395b91c9fa4470013b69dfd29072cf4d3cb0
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-01 12:40:30 +0200

    Update

--- components/StaticData.tsx
+++ components/StaticData.tsx
@@ -4,6 +4,7 @@ import { slow } from '@/utils/slow';
 export default async function StaticData() {
   const data = fetch('https://jsonplaceholder.typicode.com/posts').then(async res => {
     await slow(700);
+    console.log('Static data fetched');
     res.json();
   });
 
