5656b298b0502655372f5de5e869158f59d2e99b
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-01 12:39:09 +0200

    Test ppr with fetch

--- components/StaticData.tsx
+++ components/StaticData.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { getStaticData } from '@/data/getData';
+import { slow } from '@/utils/slow';
 
 export default async function StaticData() {
-  const data = await getStaticData();
+  const data = fetch('https://jsonplaceholder.typicode.com/posts').then(async res => {
+    await slow(700);
+    res.json();
+  });
 
   return (
     <div className="bg-blue-500 p-4 text-white">
       Statisk data
-      {data[0].id}
+      {data.toString()}
     </div>
   );
 }
