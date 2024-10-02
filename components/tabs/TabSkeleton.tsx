e5966c4256cb6795f547e5a9775c58e8e69f1cc9
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-02 19:50:43 +0200

    Update css

--- components/tabs/TabSkeleton.tsx
+++ components/tabs/TabSkeleton.tsx
@@ -4,9 +4,9 @@ import Skeleton from '../ui/Skeleton';
 export function TabsSkeleton() {
   return (
     <div className="grid w-full grid-cols-3 gap-6">
-      <Skeleton className="h-40" />
-      <Skeleton className="h-40" />
-      <Skeleton className="h-40" />
+      <Skeleton className="h-48" />
+      <Skeleton className="h-48" />
+      <Skeleton className="h-48" />
     </div>
   );
 }
