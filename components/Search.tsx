47b5b0f75e1d635e343861bd9474176d3043d593
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-02 21:54:32 +0200

    Update

--- components/Search.tsx
+++ components/Search.tsx
@@ -47,5 +47,5 @@ export default function Search() {
 }
 
 export function SearchSkeleton() {
-  return <input className="mt-8 w-full sm:w-96" placeholder="Loading..." disabled />;
+  return <input className="mt-7 w-full sm:w-96" placeholder="Loading..." disabled />;
 }
