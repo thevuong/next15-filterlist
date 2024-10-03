07670d207377daba5eb561d60d301ca23c93725f
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-03 21:49:53 +0200

    try no slow

--- utils/slow.ts
+++ utils/slow.ts
@@ -1,5 +1,6 @@
 export async function slow(delay: number = 1000) {
+  console.log(`Sleeping for ${delay}ms`);
   await new Promise(resolve => {
-    return setTimeout(resolve, delay);
+    return setTimeout(resolve, 0);
   });
 }
