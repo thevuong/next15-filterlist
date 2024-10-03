eb8329ea3cefe2766b674452178db8602fe783a3
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-03 21:56:37 +0200

    Add scroll false

--- components/tabs/Tab.tsx
+++ components/tabs/Tab.tsx
@@ -14,6 +14,7 @@ type Props = {
 export default function Tab({ tabId, children, activeTab, header }: Props) {
   return (
     <Link
+      scroll={false}
       className={cn(
         activeTab === tabId ? 'border-primary bg-primary-light' : 'border-transparent bg-primary-lighter',
         'flex w-full min-w-fit flex-col gap-3 border-b-4 p-3 -outline-offset-2 hover:bg-primary-light focus:outline-2 focus:outline-primary sm:gap-6 sm:p-6',
