775ec04ed5c2aec10802ae87ebcc9685182c8aac
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-06 17:31:46 +0200

    Simplify togglegroup

--- components/CategoryFilter.tsx
+++ components/CategoryFilter.tsx
@@ -5,22 +5,12 @@ import React, { use } from 'react';
 import ToggleGroup from './ui/toggle-group/ToggleGroup';
 import type { Category } from '@prisma/client';
 
-const mapCategories = (categories: Category[]) => {
-  return Object.values(categories).map(category => {
-    return {
-      label: category.name,
-      value: category.id.toString(),
-    };
-  });
-};
-
 type Props = {
   categoriesPromise: Promise<Record<string, Category>>;
 };
 
 export default function CategoryFilter({ categoriesPromise }: Props) {
   const categoriesMap = use(categoriesPromise);
-  const categories = Object.values(categoriesMap);
   const searchParams = useSearchParams();
   const router = useRouter();
   const selectedCategories = searchParams.getAll('category');
@@ -28,16 +18,14 @@ export default function CategoryFilter({ categoriesPromise }: Props) {
   return (
     <div className="flex flex-wrap gap-2">
       <ToggleGroup
-        options={mapCategories(categories)}
-        selectedOptions={mapCategories(
-          selectedCategories.map(id => {
-            return categoriesMap[id];
-          }),
-        )}
-        onToggle={newOptions => {
-          const newCategories = newOptions.map(option => {
-            return option.value;
-          });
+        options={Object.values(categoriesMap).map(category => {
+          return {
+            label: category.name,
+            value: category.id.toString(),
+          };
+        })}
+        selectedValues={selectedCategories}
+        onToggle={newCategories => {
           const params = new URLSearchParams(searchParams);
           params.delete('category');
           newCategories.forEach(category => {
