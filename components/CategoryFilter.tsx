c716b3c255cbd13c17e5dbd3ced0623d426523f5
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-06 17:46:55 +0200

    Update

--- components/CategoryFilter.tsx
+++ components/CategoryFilter.tsx
@@ -16,24 +16,26 @@ export default function CategoryFilter({ categoriesPromise }: Props) {
   const selectedCategories = searchParams.getAll('category');
 
   return (
-    <ToggleGroup
-      options={Object.values(categoriesMap).map(category => {
-        return {
-          label: category.name,
-          value: category.id.toString(),
-        };
-      })}
-      selectedValues={selectedCategories}
-      onToggle={newCategories => {
-        const params = new URLSearchParams(searchParams);
-        params.delete('category');
-        newCategories.forEach(category => {
-          return params.append('category', category);
-        });
-        router.push(`?${params.toString()}`, {
-          scroll: false,
-        });
-      }}
-    />
+    <div>
+      <ToggleGroup
+        options={Object.values(categoriesMap).map(category => {
+          return {
+            label: category.name,
+            value: category.id.toString(),
+          };
+        })}
+        selectedValues={selectedCategories}
+        onToggle={newCategories => {
+          const params = new URLSearchParams(searchParams);
+          params.delete('category');
+          newCategories.forEach(category => {
+            return params.append('category', category);
+          });
+          router.push(`?${params.toString()}`, {
+            scroll: false,
+          });
+        }}
+      />
+    </div>
   );
 }
