e6143baa671907e02b5ea904e2cf479967baaf88
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-10 08:14:23 +0200

    Move searchStatus to a ui component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,28 +1,6 @@
-'use client';
-
 import React from 'react';
 import type { TaskStatus } from '@/types/task';
-import { SearchIcon } from './ui/icons/SearchIcon';
-import { SpinnerIcon } from './ui/icons/SpinnerIcon';
-
-function SearchStatus({ searching }: { searching: boolean }) {
-  return (
-    <div
-      aria-label={searching ? 'Searching...' : 'Search'}
-      aria-live="polite"
-      aria-busy={searching}
-      className="absolute left-4 top-[41px]"
-    >
-      {searching ? (
-        <div className="h-fit w-fit animate-spin">
-          <SpinnerIcon aria-hidden="true" width={16} height={16} className="text-gray" />
-        </div>
-      ) : (
-        <SearchIcon aria-hidden="true" width={16} height={16} className="text-gray" />
-      )}
-    </div>
-  );
-}
+import SearchStatus from './ui/SearchStatus';
 
 export default function Search() {
   return (
